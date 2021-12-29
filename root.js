8abc054e1405338c34aa756f449cc6b277d94aec
teletha <teletha@users.noreply.github.com>
2021-12-29 05:37:06 +0000

    deploy: b8b895d1ee90bb56999be8628614d4b39c9e0b31

---
+++ root.js
@@ -0,0 +1,30 @@
+const root = {
+	"docs": [],
+	"modules": [],
+	"packages": [
+		"aigis"
+	],
+	"types": [
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Unit",
+			"packageName": "aigis",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "UnitBuilder",
+			"packageName": "aigis",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Units",
+			"packageName": "aigis",
+			"type": "Class"
+		}
+	]
+}
